1d947d70c69a29d1d3b5cc4b145a94c7dbed2c8b
Matheus Prata <matheusprata.ti@gmail.com>
2023-06-02 10:26:26 -0300

    criação do primeiro mapeamento e teste

--- src/main/java/com/matheusprata/escola/EscolaApplication.java
+++ src/main/java/com/matheusprata/escola/EscolaApplication.java
@@ -2,12 +2,14 @@ package com.matheusprata.escola;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
+@RequestMapping("/")
 public class EscolaApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(EscolaApplication.class, args);
 	}
-
 }
