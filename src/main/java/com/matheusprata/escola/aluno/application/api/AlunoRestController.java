e7c3d843c4ded6595db92a85aa5623e4ebe7329c
Matheus Prata <matheusprata.ti@gmail.com>
2023-06-05 10:38:18 -0300

    criação do AlunoRestController

---
+++ src/main/java/com/matheusprata/escola/aluno/application/api/AlunoRestController.java
@@ -0,0 +1,12 @@
+package com.matheusprata.escola.aluno.application.api;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@Log4j2
+@RequiredArgsConstructor
+public class AlunoRestController implements AlunoApi{
+
+}
