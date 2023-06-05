20db0025ee9e26ceaf046256041c52bfb281af07
Matheus Prata <matheusprata.ti@gmail.com>
2023-06-05 16:06:33 -0300

    adicionado handler ao projeto da escola

---
+++ src/main/java/com/matheusprata/escola/handler/RestResponseEntityExceptionHandler.java
@@ -0,0 +1,50 @@
+package com.matheusprata.escola.handler;
+
+import lombok.extern.log4j.Log4j2;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.multipart.MaxUploadSizeExceededException;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestControllerAdvice
+@Log4j2
+public class RestResponseEntityExceptionHandler {
+	@ExceptionHandler(APIException.class)
+	public ResponseEntity<ErrorApiResponse> handlerGenericException(APIException ex) {
+		return ex.buildErrorResponseEntity();
+	}
+
+	@ExceptionHandler(Exception.class)
+	public ResponseEntity<ErrorApiResponse> handlerGenericException(Exception ex) {
+		log.error("Exception: ", ex);
+		return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+				.body(ErrorApiResponse.builder().description("INTERNAL SERVER ERROR!")
+						.message("POR FAVOR INFORME AO ADMINISTRADOR DO SISTEMA!").build());
+	}
+
+	@ResponseStatus(HttpStatus.BAD_REQUEST)
+	@ExceptionHandler(MethodArgumentNotValidException.class)
+	public Map<String, String> handleValidationExceptions(MethodArgumentNotValidException ex) {
+		Map<String, String> errors = new HashMap<>();
+		ex.getBindingResult().getAllErrors().forEach((error) -> {
+			String fieldName = ((FieldError) error).getField();
+			String errorMessage = error.getDefaultMessage();
+			errors.put(fieldName, errorMessage);
+		});
+		return errors;
+	}
+
+	@ExceptionHandler(MaxUploadSizeExceededException.class)
+	public ResponseEntity<ErrorResponse> handleMaxUploadSizeExceededException(MaxUploadSizeExceededException ex) {
+		String mensagem = "O tamanho do arquivo enviado excede o limite permitido de 1 MB.";
+		ErrorResponse erro = new ErrorResponse(HttpStatus.BAD_REQUEST.value(), mensagem);
+		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(erro);
+	}
+}
