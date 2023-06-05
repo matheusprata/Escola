0796c4a27fce371a4efab60fa2d2535d7fb5dcb8
Matheus Prata <matheusprata.ti@gmail.com>
2023-06-05 11:39:02 -0300

    deixado enum sexo no domain

--- src/main/java/com/matheusprata/escola/aluno/application/api/Sexo.java
+++
@@ -1,5 +0,0 @@
-package com.matheusprata.escola.aluno.application.api;
-
-public enum Sexo {
-    MASCULINO, FEMININO;
-}
---
+++ src/main/java/com/matheusprata/escola/aluno/domain/Sexo.java
@@ -0,0 +1,5 @@
+package com.matheusprata.escola.aluno.domain;
+
+public enum Sexo {
+    MASCULINO, FEMININO;
+}
