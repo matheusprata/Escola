8d77c9284b9b2b71b21ca6626fca31c263dc170b
Matheus Prata <matheusprata.ti@gmail.com>
2023-06-05 15:31:50 -0300

    criação do get ListResponse

---
+++ src/main/java/com/matheusprata/escola/aluno/application/api/AlunoListResponse.java
@@ -0,0 +1,40 @@
+package com.matheusprata.escola.aluno.application.api;
+
+import com.matheusprata.escola.aluno.domain.Aluno;
+import com.matheusprata.escola.aluno.domain.Sexo;
+import lombok.Value;
+import java.time.LocalDate;
+import java.util.List;
+import java.util.UUID;
+import java.util.stream.Collectors;
+
+@Value
+public class AlunoListResponse {
+    UUID idAluno;
+    String cpf;
+    String nomeCompleto;
+    String turma;
+    String email;
+    String celular;
+    Sexo sexo;
+    LocalDate dataNascimento;
+    LocalDate dataMatricula;
+
+    public static List<AlunoListResponse> converte(List<Aluno> alunos){
+        return alunos.stream()
+                .map(AlunoListResponse::new)
+                .collect(Collectors.toList());
+    }
+
+    public AlunoListResponse(Aluno aluno) {
+        this.idAluno = aluno.getIdAluno();
+        this.cpf = aluno.getCpf();
+        this.nomeCompleto = aluno.getNomeCompleto();
+        this.turma = aluno.getTurma();
+        this.email = aluno.getEmail();
+        this.celular = aluno.getCelular();
+        this.sexo = aluno.getSexo();
+        this.dataNascimento = aluno.getDataNascimento();
+        this.dataMatricula = aluno.getDataMatricula();
+    }
+}
