6bc993346c9399ddeacc4204b01942f0d50ff6d6
Matheus Prata <matheusprata.ti@gmail.com>
2023-06-05 15:30:47 -0300

    adicionado get no ApplicationService e Repository

--- src/main/java/com/matheusprata/escola/aluno/application/repository/AlunoRepository.java
+++ src/main/java/com/matheusprata/escola/aluno/application/repository/AlunoRepository.java
@@ -1,7 +1,9 @@
 package com.matheusprata.escola.aluno.application.repository;
 
 import com.matheusprata.escola.aluno.domain.Aluno;
+import java.util.List;
 
 public interface AlunoRepository {
     Aluno saveAluno(Aluno aluno);
+    List<Aluno> getAllAlunos();
 }
--- src/main/java/com/matheusprata/escola/aluno/application/service/AlunoApplicationService.java
+++ src/main/java/com/matheusprata/escola/aluno/application/service/AlunoApplicationService.java
@@ -1,12 +1,15 @@
 package com.matheusprata.escola.aluno.application.service;
 
 import com.matheusprata.escola.aluno.application.api.AlunoIdResponse;
+import com.matheusprata.escola.aluno.application.api.AlunoListResponse;
 import com.matheusprata.escola.aluno.application.api.AlunoRequest;
 import com.matheusprata.escola.aluno.application.repository.AlunoRepository;
 import com.matheusprata.escola.aluno.domain.Aluno;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @Log4j2
@@ -20,5 +23,13 @@ public class AlunoApplicationService implements AlunoService{
         Aluno aluno = alunoRepository.saveAluno(new Aluno(alunoRequest));
         log.info("[finaliza] AlunoApplicationService - saveAluno");
         return AlunoIdResponse.builder().idAluno(aluno.getIdAluno()).build();
+    }
+
+    @Override
+    public List<AlunoListResponse> getAllAlunos() {
+        log.info("[inicia] AlunoApplicationService - getAllAlunos");
+        List<Aluno> alunos = alunoRepository.getAllAlunos();
+        log.info("[finaliza] AlunoApplicationService - getAllAlunos");
+        return AlunoListResponse.converte(alunos);
     }
 }
