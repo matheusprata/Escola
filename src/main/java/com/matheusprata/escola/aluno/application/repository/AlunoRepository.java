b6d40e2d99b632b5366183737beeb9cdad4c175a
Matheus Prata <matheusprata.ti@gmail.com>
2023-06-07 16:29:41 -0300

    adicionado get para localizar aluno por id direto no repository

--- src/main/java/com/matheusprata/escola/aluno/application/repository/AlunoRepository.java
+++ src/main/java/com/matheusprata/escola/aluno/application/repository/AlunoRepository.java
@@ -3,9 +3,11 @@ package com.matheusprata.escola.aluno.application.repository;
 import com.matheusprata.escola.aluno.domain.Aluno;
 import java.util.List;
 import java.util.Optional;
+import java.util.UUID;
 
 public interface AlunoRepository {
     Aluno saveAluno(Aluno aluno);
     List<Aluno> getAllAlunos();
     Optional<Aluno> findByCpf(String cpf);
+    Aluno getOneAluno(UUID idAluno);
 }
