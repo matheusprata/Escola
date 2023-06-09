ad5bd34c8d73124d95c63b5ac28862e205c66600
matheusprata <matheusprata.ti@gmail.com>
2023-06-09 11:42:05 -0300

    implementação no applicationService e na interface service

--- src/main/java/com/matheusprata/escola/responsavel/application/service/ResponsavelApplicationService.java
+++ src/main/java/com/matheusprata/escola/responsavel/application/service/ResponsavelApplicationService.java
@@ -2,6 +2,7 @@ package com.matheusprata.escola.responsavel.application.service;
 
 import com.matheusprata.escola.aluno.application.repository.AlunoRepository;
 import com.matheusprata.escola.aluno.domain.Aluno;
+import com.matheusprata.escola.responsavel.application.api.ResponsavelListResponse;
 import com.matheusprata.escola.responsavel.application.api.ResponsavelRequest;
 import com.matheusprata.escola.responsavel.application.api.ResponsavelResponse;
 import com.matheusprata.escola.responsavel.application.repository.ResponsavelRepository;
@@ -9,6 +10,8 @@ import com.matheusprata.escola.responsavel.domain.Responsavel;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 import java.util.UUID;
 
 @Service
@@ -25,5 +28,13 @@ public class ResponsavelApplicationService implements ResponsavelService{
         Responsavel responsavel = responsavelRepository.saveResponsavel(new Responsavel(aluno, responsavelRequest));
         log.info("[finaliza] ResponsavelApplicationService - saveResponsavel");
         return new ResponsavelResponse(responsavel);
+    }
+
+    @Override
+    public List<ResponsavelListResponse> getAllResponsaveis() {
+        log.info("[inicia] ResponsavelApplicationService - getAllResponsaveis");
+        List<Responsavel> responsaveis = responsavelRepository.getAllResponsaveis();
+        log.info("[finaliza] ResponsavelApplicationService - getAllResponsaveis");
+        return ResponsavelListResponse.converte(responsaveis);
     }
 }
--- src/main/java/com/matheusprata/escola/responsavel/application/service/ResponsavelService.java
+++ src/main/java/com/matheusprata/escola/responsavel/application/service/ResponsavelService.java
@@ -1,9 +1,12 @@
 package com.matheusprata.escola.responsavel.application.service;
 
+import com.matheusprata.escola.responsavel.application.api.ResponsavelListResponse;
 import com.matheusprata.escola.responsavel.application.api.ResponsavelRequest;
 import com.matheusprata.escola.responsavel.application.api.ResponsavelResponse;
+import java.util.List;
 import java.util.UUID;
 
 public interface ResponsavelService {
-    ResponsavelResponse saveResponsavel(UUID idAluno, ResponsavelRequest responsavelRequest);
+       ResponsavelResponse saveResponsavel(UUID idAluno, ResponsavelRequest responsavelRequest);
+    List<ResponsavelListResponse> getAllResponsaveis();
 }
