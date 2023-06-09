bd0cb258613d81265de49bfd0000652ca97216ae
matheusprata <matheusprata.ti@gmail.com>
2023-06-09 14:26:07 -0300

    implementação do getOneResponsavel

--- src/main/java/com/matheusprata/escola/responsavel/infra/ResponsavelInfraRepository.java
+++ src/main/java/com/matheusprata/escola/responsavel/infra/ResponsavelInfraRepository.java
@@ -1,11 +1,15 @@
 package com.matheusprata.escola.responsavel.infra;
 
+import com.matheusprata.escola.handler.APIException;
 import com.matheusprata.escola.responsavel.application.repository.ResponsavelRepository;
 import com.matheusprata.escola.responsavel.domain.Responsavel;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Repository;
+
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
@@ -28,5 +32,14 @@ public class ResponsavelInfraRepository implements ResponsavelRepository {
         List<Responsavel> responsaveis = responsavelSpringDataJPARepository.findAll();
         log.info("[finaliza] ResponsavelInfraRepository - getAllResponsavel");
         return responsaveis;
+    }
+
+    @Override
+    public Responsavel getOneResponsavel(UUID idResponsavel) {
+        Optional<Responsavel> optionalResponsavel = responsavelSpringDataJPARepository.findById(idResponsavel);
+        Responsavel responsavel = optionalResponsavel
+                .orElseThrow(() -> APIException.build(HttpStatus.BAD_REQUEST,
+                        "Contato do responsavel pelo aluno não encontrado!"));
+        return responsavel;
     }
 }
