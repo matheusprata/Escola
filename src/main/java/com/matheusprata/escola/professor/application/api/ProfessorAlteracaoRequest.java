7e9bb1f28f318c43aafc2f35e1b7a12f90e72dbc
Matheus Prata <matheusprata.ti@gmail.com>
2023-08-07 11:29:34 -0300

    criação da classe ProfessorAlteracaoRequest

---
+++ src/main/java/com/matheusprata/escola/professor/application/api/ProfessorAlteracaoRequest.java
@@ -0,0 +1,11 @@
+package com.matheusprata.escola.professor.application.api;
+
+import lombok.Value;
+
+@Value
+public class ProfessorAlteracaoRequest {
+    String nomeCompleto;
+    String turma;
+    String email;
+    String celular;
+}
