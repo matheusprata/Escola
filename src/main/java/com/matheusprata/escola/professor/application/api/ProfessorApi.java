a9b42bcaae7b90e83dbfd664b8b67d1f6a7c159b
Matheus Prata <matheusprata.ti@gmail.com>
2023-08-07 09:44:11 -0300

    implementa busca por todos os professores

--- src/main/java/com/matheusprata/escola/professor/application/api/ProfessorApi.java
+++ src/main/java/com/matheusprata/escola/professor/application/api/ProfessorApi.java
@@ -3,10 +3,9 @@ package com.matheusprata.escola.professor.application.api;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @Tag(name = "Professor", description = "Professor APIs")
 @RequestMapping("/v1/professor")
@@ -15,4 +14,8 @@ public interface ProfessorApi {
     @PostMapping
     @ResponseStatus(code = HttpStatus.CREATED)
     ProfessorIdResponse saveProfessor(@Valid @RequestBody ProfessorRequest professorRequest);
+
+    @GetMapping("/all")
+    @ResponseStatus(code = HttpStatus.OK)
+    List<ProfessorListResponse> getAllProfessores();
 }
