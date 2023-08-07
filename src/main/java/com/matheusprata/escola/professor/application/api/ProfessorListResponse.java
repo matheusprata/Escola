0cf119b8f400b9f7865a50db4035740bfa644bbc
Matheus Prata <matheusprata.ti@gmail.com>
2023-08-07 10:04:18 -0300

    redução da list pois agora temos a classe detalhadoresponse quando precisar de mais informações

--- src/main/java/com/matheusprata/escola/professor/application/api/ProfessorListResponse.java
+++ src/main/java/com/matheusprata/escola/professor/application/api/ProfessorListResponse.java
@@ -10,19 +10,13 @@ import java.util.stream.Collectors;
 @Value
 public class ProfessorListResponse {
     UUID idProfessor;
-    String cpf;
     String nomeCompleto;
     String turma;
-    String email;
-    String celular;
 
     public ProfessorListResponse (Professor professor){
         this.idProfessor = professor.getIdProfessor();
-        this.cpf = professor.getCpf();
         this.nomeCompleto = professor.getNomeCompleto();
         this.turma = professor.getTurma();
-        this.email = professor.getEmail();
-        this.celular = professor.getCelular();
     }
     public static List<ProfessorListResponse> converte(List<Professor>professores){
         return professores.stream()
