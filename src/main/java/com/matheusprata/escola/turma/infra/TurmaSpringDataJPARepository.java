adffc9eb7d668cf99bceaa4b5d49a0ce5166e754
Matheus Prata <matheusprata.ti@gmail.com>
2023-08-07 16:42:11 -0300

    implementado post turma, na nova classe TurmaInfraRepository e criado TurmaSpringDataJPARepository

---
+++ src/main/java/com/matheusprata/escola/turma/infra/TurmaSpringDataJPARepository.java
@@ -0,0 +1,7 @@
+package com.matheusprata.escola.turma.infra;
+
+import com.matheusprata.escola.turma.domain.Turma;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TurmaSpringDataJPARepository extends JpaRepository<Turma, Long> {
+}
