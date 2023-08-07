ef404279f01e68a6bf3363ef418fd191c2ee75ae
Matheus Prata <matheusprata.ti@gmail.com>
2023-08-07 09:50:59 -0300

    implementa get all na classe ProfessorApplicationService e ProfessorRepository

--- src/main/java/com/matheusprata/escola/professor/application/repository/ProfessorRepository.java
+++ src/main/java/com/matheusprata/escola/professor/application/repository/ProfessorRepository.java
@@ -2,6 +2,9 @@ package com.matheusprata.escola.professor.application.repository;
 
 import com.matheusprata.escola.professor.domain.Professor;
 
+import java.util.List;
+
 public interface ProfessorRepository {
     Professor saveProfessor(Professor professor);
+    List<Professor> getAllProfessores();
 }
--- src/main/java/com/matheusprata/escola/professor/application/service/ProfessorApplicationService.java
+++ src/main/java/com/matheusprata/escola/professor/application/service/ProfessorApplicationService.java
@@ -1,12 +1,15 @@
 package com.matheusprata.escola.professor.application.service;
 
 import com.matheusprata.escola.professor.application.api.ProfessorIdResponse;
+import com.matheusprata.escola.professor.application.api.ProfessorListResponse;
 import com.matheusprata.escola.professor.application.api.ProfessorRequest;
 import com.matheusprata.escola.professor.application.repository.ProfessorRepository;
 import com.matheusprata.escola.professor.domain.Professor;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -20,5 +23,13 @@ public class ProfessorApplicationService implements ProfessorService{
         Professor professor = professorRepository.saveProfessor(new Professor(professorRequest));
         log.info("[finaliza] ProfessorApplicationService - saveProfessor");
         return ProfessorIdResponse.builder().idProfessor(professor.getIdProfessor()).build();
+    }
+
+    @Override
+    public List<ProfessorListResponse> getAllProfessores() {
+        log.info("[inicia] ProfessorApplicationService - getAllProfessores");
+        List<Professor> professores = professorRepository.getAllProfessores();
+        log.info("[finaliza] ProfessorApplicationService - getAllProfessores");
+        return ProfessorListResponse.converte(professores);
     }
 }
