bbd1f39e07a4247967dd6778f73c6fd38103b4a9
Matheus Prata <matheusprata.ti@gmail.com>
2023-08-07 09:48:12 -0300

    implementa get all na classe controller e service

--- src/main/java/com/matheusprata/escola/professor/application/api/ProfessorRestController.java
+++ src/main/java/com/matheusprata/escola/professor/application/api/ProfessorRestController.java
@@ -5,6 +5,8 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 @Log4j2
 @RequiredArgsConstructor
@@ -17,5 +19,13 @@ public class ProfessorRestController implements ProfessorApi{
         ProfessorIdResponse idResponse = professorService.saveProfessor(professorRequest);
         log.info("[finaliza] ProfessorRestController - saveProfessor");
         return idResponse;
+    }
+
+    @Override
+    public List<ProfessorListResponse> getAllProfessores() {
+        log.info("[inicia] ProfessorRestController - getAllProfessores");
+        List<ProfessorListResponse> professores = professorService.getAllProfessores();
+        log.info("[inicia] ProfessorRestController - getAllProfessores");
+        return professores;
     }
 }
--- src/main/java/com/matheusprata/escola/professor/application/service/ProfessorService.java
+++ src/main/java/com/matheusprata/escola/professor/application/service/ProfessorService.java
@@ -1,8 +1,12 @@
 package com.matheusprata.escola.professor.application.service;
 
 import com.matheusprata.escola.professor.application.api.ProfessorIdResponse;
+import com.matheusprata.escola.professor.application.api.ProfessorListResponse;
 import com.matheusprata.escola.professor.application.api.ProfessorRequest;
+
+import java.util.List;
 
 public interface ProfessorService {
     ProfessorIdResponse saveProfessor(ProfessorRequest professorRequest);
+    List<ProfessorListResponse> getAllProfessores();
 }
